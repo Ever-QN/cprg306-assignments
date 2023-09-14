04cbd6d2e7e5ec81b8e1d6c0796d03cbe324dfdb
Quynh Ngo <QuynhTNgo@live.com>
2023-09-14 10:08:11 -0600

    Create week 2 page.js

---
+++ cprg306-assignments/app/week2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "../StudentInfo";
+
+export default function Page() {
+    return (
+        <div class='week-two'>
+            <h1>My Shopping List</h1>
+            <StudentInfo/>
+        </div>
+    )
+}
