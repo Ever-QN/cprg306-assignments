86aa64ffb8167628e3e234f9a96f2293ddb07c0a
Quynh Ngo <QuynhTNgo@live.com>
2023-09-15 13:28:17 -0600

    Reformat StudentInfo.jsx

--- app/StudentInfo.jsx
+++ app/StudentInfo.jsx
@@ -2,10 +2,10 @@ import Link from "next/link"
 
 export default function StudentInfo() {
     return (
-        <div class='student-info-container'>
+        <>
             <p>Name: Quynh Ngo</p>
             <p>Course section: CPRG 306 A</p>
-            <Link href='https://github.com/Ever-QN/cprg306-assignments'>Github Repository Link (Click me)</Link>
-        </div>
+            <Link href='https://github.com/Ever-QN/cprg306-assignments'>Github Repository Link (Click me)</Link> <br />
+        </>
     )
 }
