fd5cec92ada787001f323740c3b9dd8f5d4e069e
Quynh Ngo <QuynhTNgo@live.com>
2023-09-15 13:28:42 -0600

    change class to className, add route back to home

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import StudentInfo from "../StudentInfo";
 
 export default function Page() {
     return (
-        <div class='week-two'>
+        <div className='week-two'>
+            <p><Link href='/'>Home</Link></p>
             <h1>My Shopping List</h1>
             <StudentInfo/>
         </div>
