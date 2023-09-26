2c78d012e886ba23f6e4af52d1f1760e9d92b945
Quynh Ngo <QuynhTNgo@live.com>
2023-09-26 17:29:50 -0600

    Remove constants, manually add in components

--- app/week3/item-list.js
+++ app/week3/item-list.js
@@ -73,22 +73,21 @@ export default function ItemList() {
         category: "household",
       };
 
-      const items = {item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12};
-
-      const itemList = [];
-
-      for (let i = 0; i < items.length; i++) {
-        itemList.push([items.name, items.quantity, items.category]);
-      }
-
     return (
-        <main>
-            <h1 className='text-xl font-bold px-4 py-2'>Shopping List</h1>
-            <li className='list-none'>
-                <ul className='p-2 m-4 bg-slate-900 max-w-sm'>
-
-                </ul>
-            </li>
+        <main className="bg-slate-950">
+          <h1 className='text-4xl font-bold px-4 py-2'>Shopping List</h1>
+          <Item name={item1.name} quantity={item1.quantity} category={item1.category}></Item>
+          <Item name={item2.name} quantity={item2.quantity} category={item2.category}></Item>
+          <Item name={item3.name} quantity={item3.quantity} category={item3.category}></Item>
+          <Item name={item4.name} quantity={item4.quantity} category={item4.category}></Item>
+          <Item name={item5.name} quantity={item5.quantity} category={item5.category}></Item>
+          <Item name={item6.name} quantity={item6.quantity} category={item6.category}></Item>
+          <Item name={item7.name} quantity={item7.quantity} category={item7.category}></Item>
+          <Item name={item8.name} quantity={item8.quantity} category={item8.category}></Item>
+          <Item name={item9.name} quantity={item9.quantity} category={item9.category}></Item>
+          <Item name={item10.name} quantity={item10.quantity} category={item10.category}></Item>
+          <Item name={item11.name} quantity={item11.quantity} category={item11.category}></Item>
+          <Item name={item12.name} quantity={item12.quantity} category={item12.category}></Item>
         </main>
     )
 }
