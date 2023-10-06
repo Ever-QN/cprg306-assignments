5021aa22c3fce7b517879ac98ee9d84ea0547712
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-06 12:33:32 -0600

    Create new-item.js and page.js in week 4

---
+++ app/week4/new-item.js
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from "react";
+
+export default function NewItem() {
+
+    const [name, setName] = useState('');
+    const [quantity, setQuantity] = useState(1);
+
+    return (
+        <div>ha</div>
+    )
+}
