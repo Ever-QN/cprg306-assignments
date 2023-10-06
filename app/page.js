40ab0cd2f6f397c49c5f455329f32fe72c21bdb2
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-06 12:32:59 -0600

    Create link to week4

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function Home() {
       <main className='px-4 py-2'>
         <StudentInfo/>
         <Link href='/week2'>Week 2</Link><br />
-        <Link href='/week3'>Week 3</Link>
+        <Link href='/week3'>Week 3</Link> <br />
+        <Link href='/week4'>Week 4</Link>
       </main>
     </>
 
