935b129960a11ef9610415e4863785280e31787c
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-07 16:26:24 -0600

    Create category state, handleSubmit(), resetStates(), and form with name input

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -6,8 +6,42 @@ export default function NewItem() {
 
     const [name, setName] = useState('');
     const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState('produce');
+
+    const resetStates = () => {
+        setName('');
+        setQuantity(1);
+        setCategory('produce');
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const item = {name, quantity, category};
+
+        console.log(item);
+        alert(`Item: ${item.name}\nQuantity: ${item.quantity}\nCategory: ${item.category}`);
+
+        resetStates();
+    }
+
+
 
     return (
-        <div>ha</div>
+        <form onSubmit={handleSubmit} className='max-w-md mx-auto mt-8 p-4 border rounded-lg shadow-lg'>
+        <div className='mb-4'>
+            <label htmlFor="name" className=''>Name: </label>
+            <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+            className='border border-gray-300 px-3 py-2 rounded-md w-full focus:outline-none focus:ring focus:border-blue-300 text-black'
+            />
+        </div>
+
+    </form>
     )
 }
