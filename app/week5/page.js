445147dcad501d8f6c278d9aaf17bd870ddaf48f
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-16 19:47:55 -0600

    Remove classname for main and create underline on hover

--- app/week5/page.js
+++ app/week5/page.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <main className='week3'>
-            <p className="px-4 bg-slate-700"><Link href='/'>Home</Link></p>
+        <main>
+            <p className="px-4 mb-4 bg-slate-700 hover:underline"><Link href='/'>Home</Link></p>
             <ItemList />
         </main>
     )
