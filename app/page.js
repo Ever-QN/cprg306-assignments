c93fe0d4d6fc3f22948652dc9325d7b6f7ea0596
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-16 19:33:56 -0600

    Add link to week5

--- app/page.js
+++ app/page.js
@@ -7,9 +7,10 @@ export default function Home() {
       <h1 className='px-4 py-2 text-xl font-bold'>CPRG 306: Web Development 2 - Assignments</h1>
       <main className='px-4 py-2'>
         <StudentInfo/>
-        <Link href='/week2'>Week 2</Link><br />
+        <Link href='/week2'>Week 2</Link> <br />
         <Link href='/week3'>Week 3</Link> <br />
-        <Link href='/week4'>Week 4</Link>
+        <Link href='/week4'>Week 4</Link> <br />
+        <Link href='/week5'>Week 5</Link> <br />
       </main>
     </>
 
