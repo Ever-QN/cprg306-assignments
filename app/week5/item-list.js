23b2142329ae53f18413f0e45c524496473475ab
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-16 19:26:45 -0600

    Create week5 folder and files

---
+++ app/week5/item-list.js
@@ -0,0 +1,11 @@
+import Item from "./item";
+import { useState } from "react";
+import items from "items.json";
+
+export default function ItemList() {
+    const [sortBy, setSortBy] = useState("name");
+
+    return (
+        
+    )
+}
