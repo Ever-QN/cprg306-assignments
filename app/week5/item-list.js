ee489f44b1993975e883a8fded5b0b22e2142d67
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-16 19:49:07 -0600

    Create sortedItems, handleSortChange and import .json properly

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -1,11 +1,29 @@
+'use client';
+
 import Item from "./item";
+import items from "./items.json";
 import { useState } from "react";
-import items from "items.json";
 
 export default function ItemList() {
     const [sortBy, setSortBy] = useState("name");
 
+    const sortedItems = items.sort((a, b) => {
+      if (sortBy === 'name') {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      } else if (sortBy === 'category') {
+        if (a.category < b.category) return -1;
+        if (a.category > b.category) return 1;
+        return 0;
+      }
+    });
+
+    const handleSortChange = (newSortBy) => {
+      setSortBy(newSortBy);
+    };
+
     return (
-        
+      
     )
 }
