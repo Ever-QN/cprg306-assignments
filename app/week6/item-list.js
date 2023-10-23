d7eddc7047119ea1c2b8f1a971f5f68c7e0731c1
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-23 15:26:59 -0600

    Modify item-list to receive data via props

--- app/week6/item-list.js
+++ app/week6/item-list.js
@@ -1,13 +1,14 @@
 'use client';
 
 import Item from "./item";
-import items from "./items.json";
 import { useState } from "react";
 
-export default function ItemList() {
+export default function ItemList({ items }) {
     const [sortBy, setSortBy] = useState("name");
 
-    const sortedItems = items.sort((a, b) => {
+    const sortedItems = [...items];
+
+    sortedItems.sort((a, b) => {
       if (sortBy === 'name') {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
