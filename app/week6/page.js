d826f33e480b9777461a39af837d92b2da51ef0a
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-23 15:21:09 -0600

    Finish step 2

--- app/week6/page.js
+++ app/week6/page.js
@@ -1,11 +1,24 @@
+"use client";
+
 import ItemList from "./item-list";
+import NewItem from "./new-item.js";
+import itemsData from "./items.json";
 import Link from "next/link";
 
 export default function Page() {
+    const items = itemsData;
+
+    function handleAddItem(newItem) {
+        items.push(newItem);
+      }
+
+
     return (
         <main>
             <p className="px-4 mb-4 bg-slate-700 hover:underline"><Link href='/'>Home</Link></p>
-            <ItemList />
+            <h1>Shopping List</h1>
+            <NewItem onAddItem={handleAddItem} />
+            <ItemList items={items} />
         </main>
     )
 }
