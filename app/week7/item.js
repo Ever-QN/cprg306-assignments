4bcbcd0acc463e14a36c478c3a27deed60cae387
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-30 16:06:34 -0600

    Make each list item clickable

--- app/week7/item.js
+++ app/week7/item.js
@@ -1,9 +1,13 @@
-export default function Item({name, quantity, category}) {
+export default function Item({ name, quantity, category, onSelect }) {
+    const handleItemClick = () => {
+      onSelect();
+    };
+  
     return (
-        <li className="list-none px-4 mb-4 ml-4 mr-4 bg-slate-800">
-            <ul>Name: {name}</ul>
-            <ul>Quantity: {quantity}</ul>
-            <ul>Category: {category}</ul>
-        </li>
-    )
-}
+      <li className="list-none px-4 mb-4 ml-4 mr-4 bg-slate-800" onClick={handleItemClick}>
+        <ul>Name: {name}</ul>
+        <ul>Quantity: {quantity}</ul>
+        <ul>Category: {category}</ul>
+      </li>
+    );
+  }
