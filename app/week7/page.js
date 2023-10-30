212a2b8373952def7f5aa2bb928c358f55a710c2
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-30 16:17:47 -0600

    Filter list on ingredient click to display meals
    Currently bugged, need to display the meals associated with the ingredients.
    
    Right now I think it factors in the emoji's which causes the problem.

--- app/week7/page.js
+++ app/week7/page.js
@@ -2,6 +2,7 @@
 
 import ItemList from "./item-list";
 import NewItem from "./new-item.js";
+import MealIdeas from "./meal-ideas";
 import itemsData from "./items.json";
 import Link from "next/link";
 import { useState } from "react";
@@ -9,18 +10,31 @@ import { useState } from "react";
 export default function Page() {
 
     const [items, setItems] = useState(itemsData);
+    const [selectedItemName, setSelectedItemName] = useState('');
 
     function handleAddItem(newItem) {
         setItems([...items, newItem]);
       }
 
+    function handleItemSelect(selectedItem) {
+    const cleanedName = selectedItem.name.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|�[�-�]|�[�-�]|[\u2011-\u26FF]|�[�-�])/g, '');
+    setSelectedItemName(cleanedName);
+    }
+
 
     return (
         <main>
-            <p className="px-4 mb-4 bg-slate-700 hover:underline"><Link href='/'>Home</Link></p>
-            <h1>Shopping List</h1>
-            <NewItem onAddItem={handleAddItem} />
-            <ItemList items={items} />
+            <div className="flex justify-around">
+                <div className="bg-slate-900 px-4">
+                    <p className="px-4 mb-4 bg-slate-700 hover:underline"><Link href="/">Home</Link></p>
+                    <h1>Shopping List</h1>
+                    <NewItem onAddItem={handleAddItem} />
+                    <ItemList items={items} onItemSelect={handleItemSelect} />
+                </div>
+                <div className="bg-slate-900 px-4">
+                    <MealIdeas ingredient={selectedItemName} />
+                </div>
+            </div>
         </main>
     )
 }
