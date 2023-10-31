adb79ba872cd8fb5cf2dc90fa50b414442c15308
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-10-30 22:06:46 -0600

    Cover a wider range of emojis in regex

--- app/week7/page.js
+++ app/week7/page.js
@@ -17,7 +17,7 @@ export default function Page() {
       }
 
     function handleItemSelect(selectedItem) {
-        const cleanedName = selectedItem.name.replace(/,.*|([\u2700-\u27BF]|[\uE000-\uF8FF]|�[�-�]|�[�-�]|[\u2011-\u26FF]|�[�-�])/g, '');
+        const cleanedName = selectedItem.name.replace(/,.*/g, '').replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{1FAB0}-\u{1FABF}\u{1FAC0}-\u{1FAFF}\u{1FAD0}-\u{1FADF}\u{1FAE0}-\u{1FAFF}\u{1F3FB}-\u{1F3FF}\u{1F004}\u{1F0CF}\u{1F004}\u{1F004}\u{1F004}\u{1F004}\u{1F005}\u{1F005}\u{1F0CF}]/gu, '');
         setSelectedItemName(cleanedName);
     }
 
