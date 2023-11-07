0fc2ff6ec2a675dcf77f22b0e3d4d42f189f7619
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-11-06 17:33:21 -0700

    Add use client to top of file

---
+++ app/week8/page.js
@@ -0,0 +1,37 @@
+'use client';
+
+import Link from 'next/link';
+import { useUserAuth } from './_utils/auth-context';
+
+export default function Page() {
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+  const handleLogin = async () => {
+    await gitHubSignIn();
+  };
+
+  const handleLogout = async () => {
+    await firebaseSignOut();
+  };
+
+  return (
+    <main>
+      {user ? (
+        <div>
+          <p>Welcome, {user.displayName} ({user.email})</p>
+          <button onClick={handleLogout}>Logout</button>
+          <p>
+            <Link href='/shopping-list'>
+              <a>Go to Shopping List</a>
+            </Link>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <p>Please log in to continue:</p>
+          <button onClick={handleLogin}>Login with GitHub</button>
+        </div>
+      )}
+    </main>
+  );
+}
