538bea63a92607e454ad760b8b90a2489dc03348
Ever-QN <71616274+Ever-QN@users.noreply.github.com>
2023-11-20 23:00:15 -0700

    Create getItems and addItem

---
+++ app/week10/_services/shopping-list-service.js
@@ -0,0 +1,42 @@
+import { db } from '../_utils/firebase';
+import { collection, getDocs, addDoc, query } from 'firebase/firestore';
+
+async function getItems() {
+    const db = getFirestore();
+
+    try {
+      const userItemsCollection = collection(db, 'users', userId, 'items');
+  
+      const q = query(userItemsCollection);
+  
+      const querySnapshot = await getDocs(q);
+  
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push({
+          itemId: doc.id,
+          data: doc.data(),
+        });
+      });
+  
+      return items;
+    } catch (error) {
+      console.error('Error getting items:', error.message);
+      throw error;
+    }
+}
+
+async function addItem(userId, newItem) {
+    const db = getFirestore();
+
+    try {
+      const userItemsCollection = collection(db, 'users', userId, 'items');
+  
+      const docRef = await addDoc(userItemsCollection, newItem);
+  
+      return docRef.id;
+    } catch (error) {
+      console.error('Error adding item:', error.message);
+      throw error;
+    }
+}
